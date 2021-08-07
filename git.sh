85c3a9c2f1f59b99032df7d9882c4c0bbe8e5c7c
PranavVS <pranuvs7@gmail.com>
2021-08-07 19:08:48 +0530

    Hello commitcomplete using bash.

--- git.sh
+++ git.sh
@@ -1,16 +1,22 @@
+counter=1;
+while [ $counter -le 19 ]
+do
 # Set a string that will contain your characters:
-str='pat'
+str='pat';
 
 # Calculate the number of characters in the string:
-strlen=${#str}
+strlen=${#str};
 
 # Get a random integer in the range 0..2 (0..strlen-1):
-let r=RANDOM*strlen/32768
+let r=RANDOM*strlen/32768;
 
 # Get one random character from the strlen-character string (str):
-touch ${str:r:1}
-sleep 5
+touch ${str:r:1};
 git add .;
 git commit -m 'Hello commitcomplete using bash.';
 git push origin main;
-rm ${str:r:1}
+rm ${str:r:1};
+((counter++));
+echo $counter;
+
+done
